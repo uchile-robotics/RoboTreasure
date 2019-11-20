f9a78617939e8a1bc546c0a683d6e9262e2e3b2b
Giovanni Pais <giom.pais@gmail.com>
2019-11-20 19:01:31 -0300

    Fix workspace. Build and Devel deleted. Directories added in gitignore

--- rt_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robotica/Gio/RoboTreasure/rt_ws/src
-# Build directory: /home/robotica/Gio/RoboTreasure/rt_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(rt_states)
