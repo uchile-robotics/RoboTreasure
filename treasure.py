60ddf17c9ff217afe521a4981c0ef3a06ba2ac42
Rodrigo Salas <rodrigosalaso@gmail.com>
2019-11-20 17:08:23 -0300

    Added first SM

---
+++ treasure.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+import rospy
+import smach
+import smach_ros
+from smach_ros import IntrospectionServer
+from std_msgs.msg import String
+
+#Robot building
+from maqui_skills import robot_factory
+from maqui_skills.capabilities.tablet import TabletControllerSkill
+
+#States
+from uchile_states.navigation.states import GoState
+from uchile_states.interaction.states import Speak
+from uchile_states.interaction.tablet_states import ShowWebpage
+#########from uchile_states.interaction.tablet_states import OperationMessage
+
+
+
+class Setup(smach.State):
+    def __init__(self,robot):
+        smach.State.__init__(self, outcomes=["succeeded"])
+        self.robot = robot
+        self.tts = self.robot.get("tts")
+        self.head = self.robot.get("neck")
+        self.track = self.robot.get("track_person")
+        self.audition = self.robot.get("audition")
+        self.knowledge = self.robot.get("knowledge")
+        self.facial_features   = self.robot.get("facial_features")
+        self.face   = self.robot.get("face")
+        # self.track = self.robot.get("track_person")
+    def execute(self,userdata):
+        # self.track.set_search(False)
+        self.tts.set_language("English")
+        # self.tts.set_speed(110)
+        self.audition.set_audio_expression(False)
+        self.knowledge.pose.delete_all()
+        self.knowledge.pose.load_from_map("receptionist.sem_map")
+        self.head.home()  
+        self.face.turn_on()
+        self.facial_features.clear_face_database()
+        self.facial_features._set_resolution(4)
+        return 'succeeded'
+
+class Subs(smach.State):
+    """docstring for subs"""
+    def __init__(self, robot):
+        smach.State.__init__(self, outcomes=["succeeded"])
+        self.robot = robot
+
+    def execute(self, userdata):
+        rospy.Subscriber("qr", String, callback)
+
+    def callback(data):
+        print data.data
+        return "succeeded"
+
+
+
+def getInstance(robot):
+    """
+    parameters of the test:
+        *Places of interaction:
+            -START
+            -RECEPTION
+            -LIVING
+
+    """
+    start_place = 'start'
+    RECEPTION_PLACE = 'start'
+    JOHN_PLACE = 'near_couch'
+
+    sm = smach.StateMachine(outcomes=['succeeded', 'aborted', 'preempted'])
+    sm.userdata.qr = ""
+    sm.userdata.page = "http://198.18.0.1:8888/"
+
+    with sm:
+
+        smach.StateMachine.add('SETUP', Setup(robot),
+            transitions={
+                'succeeded':'WEB_SHOW'
+            }
+        )
+
+        # smach.StateMachine.add('QR', Subs(robot),
+        #     transitions={
+        #         'succeeded':'WEB_SHOW'
+        #     }
+        # )
+
+        # smach.StateMachine.add('WEB_SHOW', TabletControllerSkill.show_webpage(self, url="http://198.18.0.1:8888/"),
+        #     transitions={
+        #         'succeeded':'succeeded'
+        #     }
+        # )
+
+        smach.StateMachine.add('WEB_SHOW', ShowWebpage(robot, page = "http://198.18.0.1:8888/"),
+            transitions={
+                'succeeded':'succeeded'
+            }
+        )
+
+
+
+
+        return sm
+
+if __name__ == '__main__':
+
+    rospy.init_node('TREASURE')
+    base_skills = [ 
+        "audition",
+        "marker",
+        "tablet",
+        "tabletapp",
+        'knowledge',
+        "navigation",
+        "person_detector",
+        "facial_features",
+        "l_arm",
+        "r_arm"]#,
+        #"sitting_person_detector"] not sure if we need this
+
+    extra_skills = []
+
+    robot = robot_factory.build( base_skills + extra_skills, core = True)
+
+    sm = getInstance(robot)
+    sis = IntrospectionServer('treasure', sm, '/TREASURE_SM') #Smach Viewer
+    sis.start()
+    outcome = sm.execute() # here is where the test begin
+    sis.stop()
