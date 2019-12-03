48a53599cfcb811ba92fbf4b6a02c8a33d0ac6d5
DiegoCanalesR <diego.canales.r@ing.uchile.cl>
2019-12-03 13:25:22 -0300

    Fixing final page fontsize

--- mill_tornado/static/js/millionaire_s3.js
+++ mill_tornado/static/js/millionaire_s3.js
@@ -103,21 +103,26 @@ var MillionaireModel = function(data) {
         if(self.transitioning)
             return;
         self.transitioning = true;
-
-        $("#game").fadeOut('slow', function() {
-            $("#score").html('Puntos: '+ self.money());
-            $("#score").fadeIn('slow');
-            $("#hint").html('Hola');
-            $("#hint").fadeIn('slow');
-            $("#key").html(' ');
-        })      
-
         if(index === 0){
             self.money(self.money() + 100);
 			ws.send("Yes");
+			$("#game").fadeOut('slow', function() {
+				$("#score").html('Puntos: '+ self.money());
+				$("#score").fadeIn('slow');
+				$("#hint").html('Hola');
+				$("#hint").fadeIn('slow');
+				$("#key").html(' ');
+        })      
         }
 		if(index === 1){
-			ws.send("No");				
+			ws.send("No");	
+			$("#game").fadeOut('slow', function() {
+				$("#score").html('Puntos: '+ self.money());
+				$("#score").fadeIn('slow');
+				$("#hint").html('Chao');
+				$("#hint").fadeIn('slow');
+				$("#key").html(' ');
+        })      			
 		}
 		if(index === 2){
 			ws.send("Later");
